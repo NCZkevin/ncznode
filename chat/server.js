5362b04680252cfc0e9961ede1d5c7810649f92d
NCZkevin <z1376871863@163.com>
2015-11-25 21:23:14 +0800

    second

--- chat/server.js
+++ chat/server.js
@@ -49,6 +49,11 @@ var server = http.createServer(function  (request,response) {
 });
 
 
-server.listen(3000,function () {
-    console.log("server listen");
-});
+// server.listen(3000,function () {
+//     console.log("server listen");
+// });
+
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
+
+
